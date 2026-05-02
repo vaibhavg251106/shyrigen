71d7c40a4290b8193232622985a988fc08116c42
Vaibhav Gupta <vg369621@gmail.com>
2026-05-02 15:31:53 +0530

    format doc

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
 
 const shayariList = [
-'Unse hai mohbbat iss qadar chhupta mein firta hun,Haqeeqat na sahi khwabon m toh shab-o-roz milta hun',
+  'Unse hai mohbbat iss qadar chhupta mein firta hun,Haqeeqat na sahi khwabon m toh shab-o-roz milta hun',
 
-'Hai ye khwab ya sach ho tum, Meri baahon mein kya khoob lagte ho tum',
+  'Hai ye khwab ya sach ho tum, Meri baahon mein kya khoob lagte ho tum',
 
-'Na hum keh paayenge iss dar se bichhad Jane ke, Hasrat toh yun reh jayegi na mil ske na Imkaan tha aane ka ',
+  'Na hum keh paayenge iss dar se bichhad Jane ke, Hasrat toh yun reh jayegi na mil ske na Imkaan tha aane ka ',
 
-'Seher hone tak jaaga soch kar ke wo pukaarega,Shayad ek dafa ruk kar mere dar par sila-e-Wafa chukaayega',
+  'Seher hone tak jaaga soch kar ke wo pukaarega,Shayad ek dafa ruk kar mere dar par sila-e-Wafa chukaayega',
 
-'Rasta badal deta hun kahin aankhen na mil jaye, Isliye tujhse na batlata hun kahin fir pyaar iss dil mein na khil jaye',
+  'Rasta badal deta hun kahin aankhen na mil jaye, Isliye tujhse na batlata hun kahin fir pyaar iss dil mein na khil jaye',
 
-'Tum mehtaab ho ya gul-e-shadaab ho, Kis kadar batlate tumhe kitne tum la-jwab ho'
+  'Tum mehtaab ho ya gul-e-shadaab ho, Kis kadar batlate tumhe kitne tum la-jwab ho'
 ];
 
 let index = 0;
 
-function showShayari(){
+function showShayari() {
   const box = document.getElementById('shayari');
   const count = document.getElementById('count');
-  box.style.opacity='0';
-  box.style.transform='translateY(8px)';
-  setTimeout(()=>{
+  box.style.opacity = '0';
+  box.style.transform = 'translateY(8px)';
+  setTimeout(() => {
     box.innerText = shayariList[index];
     count.innerText = (index + 1) + ' / ' + shayariList.length;
-    box.style.opacity='1';
-    box.style.transform='translateY(0)';
-  },220);
+    box.style.opacity = '1';
+    box.style.transform = 'translateY(0)';
+  }, 220);
 }
 
-function nextShayari(){
+function nextShayari() {
   index = (index + 1) % shayariList.length;
   showShayari();
 }
 
-function prevShayari(){
+function prevShayari() {
   index = (index - 1 + shayariList.length) % shayariList.length;
   showShayari();
 }
 
-function copyText(){
+function copyText() {
   navigator.clipboard.writeText(shayariList[index]);
 }
 
